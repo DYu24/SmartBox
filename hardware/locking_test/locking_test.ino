e7de219d4b7770743c61d1e40750763174c910e8
zacharybys <zachary.bys@gmail.com>
2020-02-08 17:47:20 -0500

    Box locking code testing

---
+++ hardware/locking_test/locking_test.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+
+#define LEFT_MOTOR_PIN 5
+#define RIGHT_MOTOR_PIN 6
+
+#define RIGHT_MOTOR_CLOSED 10
+#define RIGHT_MOTOR_OPEN 100
+
+#define LEFT_MOTOR_CLOSE 165
+#define LEFT_MOTOR_OPEN 70
+
+Servo left_motor;
+Servo right_motor;
+
+void setup() {
+  left_motor.attach(LEFT_MOTOR_PIN);
+  right_motor.attach(RIGHT_MOTOR_PIN);
+
+  
+}
+
+void loop() {
+  right_motor.write(RIGHT_MOTOR_CLOSED);
+  left_motor.write(LEFT_MOTOR_CLOSED);
+  delay(3000);
+  right_motor.write(RIGHT_MOTOR_OPEN);
+  left_motor.write(LEFT_MOTOR_OPEN;
+  delay(3000);
+}
