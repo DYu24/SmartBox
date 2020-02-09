8831a3529f2b818ea4c37b15bdab3aa014eb86cc
zacharybys <zachary.bys@gmail.com>
2020-02-08 21:18:24 -0500

    Flag code and IR sensor final

--- hardware/po_box/po_box.ino
+++ hardware/po_box/po_box.ino
@@ -4,8 +4,10 @@
 #define SENSORPIN 4
 #define SERVOPIN 9
 
+#define FLAG_UP 118
+
 int sensorState = 0, lastState=0;         // variable for reading the pushbutton status
-int flagPosition = -100;
+int flagPosition;
 Servo flag;
  
 void setup() {
@@ -17,21 +19,27 @@ void setup() {
 
   flag.attach(SERVOPIN);
   Serial.begin(9600);
+
+  flagPosition = 6;
 }
  
 void loop(){
   sensorState = digitalRead(SENSORPIN);
   
   if (sensorState == LOW) {
-    if (flagPosition != 90) {
-      flag.write(90);      
-    }
+      if (flagPosition != FLAG_UP) {
+        flag.write(FLAG_UP);
+        flagPosition = FLAG_UP;
+        digitalWrite(LEDPIN, HIGH);   
+      }
+      digitalWrite(LEDPIN, HIGH);      
   } 
   else {
-    if (flagPosition != 0) {
-      flag.write(0);
-    }
+      if (flagPosition != 0) {
+        Serial.print(flagPosition);
+        flag.write(0);
+        flagPosition = 0;
+        digitalWrite(LEDPIN, LOW);
+      }
   }
-  
-  lastState = sensorState;
 }
